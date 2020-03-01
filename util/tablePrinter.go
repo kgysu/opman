8ccea59a16cc4ea7c190d6616a7f95f2b2b21c49
kgysu <k.gysin@protonmail.ch>
2020-03-01 20:19:12 +0100

    add status

--- util/tablePrinter.go
+++ util/tablePrinter.go
@@ -9,20 +9,20 @@ import (
 func NewStandardTable(items []v2.OpenshiftItem) table.Writer {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"#", "Kind", "Name"})
+	t.AppendHeader(table.Row{"#", "Kind", "Name", "Status"})
 	for i, item := range items {
-		t.AppendRow(table.Row{i, item.GetKind(), item.GetName()})
+		t.AppendRow(table.Row{i, item.GetKind(), item.GetName(), item.GetStatusMessage()})
 	}
-	t.AppendFooter(table.Row{"=", "Total", len(items)})
+	t.AppendFooter(table.Row{"=", "Total", "", len(items)})
 	return t
 }
 
 func NewMonitorItemsTable(items []MonitorItem) table.Writer {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"#", "Kind", "Name"})
+	t.AppendHeader(table.Row{"#", "Kind", "Name", "Status"})
 	for i, item := range items {
-		t.AppendRow(table.Row{i, item.item.GetKind(), item.status + item.item.GetName()})
+		t.AppendRow(table.Row{i, item.item.GetKind(), item.status + item.item.GetName(), item.item.GetStatusMessage()})
 	}
 	return t
 }
